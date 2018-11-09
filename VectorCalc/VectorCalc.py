e591692b8266905cd469525151be3182d3410873
osugraytr <graytr@onid.oregonstate.edu>
2018-11-09 04:32:43 -0800

    Update VectorCalc.py

--- VectorCalc/VectorCalc.py
+++ VectorCalc/VectorCalc.py
@@ -1,32 +1,47 @@
+#By: Travis Gray 11/8/2018
+
 #	Websites: 
 #
 #		ScyPi VectorCalc: https://docs.sympy.org/latest/modules/physics/vector/vectors.html#vector
 #		
 
+import math
+
 print("This code executes before main.")
 
 # UNIT_VECTOR = (0,0,0)
-x, y, x
+#x, y, x
 
 UNIT_VECTOR = (0,0,1)
 
+def SqRt(x):
+	return(math.sqrt(int(x)))
+
+def distance(Vector_1, Vector_2, PrintToggle):
+    Vector_1 = (0,0,0)
+    Vector_2 = (2,3,5)
+    print(Vector_1)
+    (x1,y1,z1) = Vector_1
+    (x2,y2,z2) = Vector_2
+    x = 4
+    print(SqRt( math.pow(SqRt(2),2) + math.pow(SqRt(3),2) + math.pow(SqRt(5),2) ))
+
 def main():
     print("This code executes after main.")
+    distance(1, 1, 1)
+##    Vector_1 = (Scalar, Unit_Vector)
 
-    Vector_1 = (Scalar, Unit_Vector)
-
-    AddVectors(Vector_1, Vector_2)
+#    AddVectors(Vector_1, Vector_2)
 
 #Adds two vectors result will return
 def AddVectors(Vector_1, Vector_2, PrintToggle):
     print("Function B")
-    Result_Vector = Vector_1 + Vector_2
+    Result_Vector = Vector_1 + Vector_2 
     if(PrintToggle==True):
         print("Result_Vector: "+str(Result_Vector))
 
 if __name__ == '__main__':
     main()
-    functionB()
 
 ##############################################################################################################
 #
