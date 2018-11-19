cc79c1b71ffe2e70bc9595240cb9f5bab171ce1c
osugraytr <graytr@onid.oregonstate.edu>
2018-11-19 01:41:15 -0800

    Delete RandomVectorPoints.py

--- RandomVectorPoints.py
+++
@@ -1,8 +0,0 @@
-import random
-print(str(random.randint(1,101)))
-
-f = open("text.txt", "w")
-for num in range(1,100):
-	f.write("[["+str(random.randint(1,101))+","+str(random.randint(1,101))+","+str(random.randint(1,101))+"],["+str(random.randint(1,101))+","+str(random.randint(1,101))+","+str(random.randint(1,101))+"]]\n")
-
-f.close()
